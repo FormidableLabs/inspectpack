b278ab3845a5a618bd03c9034822f294749425e7
Tyler Thompson <tyler.thompson@formidable.com>
2017-03-30 13:46:36 -0700

    Output numeric ids in the parser + correct index subscript to _.find() in pattern.js

--- test/smoke.spec.js
+++ test/smoke.spec.js
@@ -1,4 +1,6 @@
 "use strict";
+
+var expect = require("chai").expect;
 var path = require("path");
 var fs = require("fs");
 
@@ -14,71 +16,104 @@ var EXTENDED_TIMEOUT = 12000;
 var basicFixturePath = path.resolve(__dirname, "fixtures/basic-lodash-object-expression.js");
 var basicFixture = fs.readFileSync(basicFixturePath, "utf8");
 
+var badBundleFixturePath = path.resolve(__dirname, "fixtures/bad-bundle/app.js");
+var badBundleFixture = fs.readFileSync(badBundleFixturePath, "utf8");
+
+var checkForErrors = function (done, assertion) {
+  try {
+    assertion();
+    done();
+  } catch (e) {
+    done(e);
+  }
+};
+
 describe("Smoke tests", function () {
-  it.only("analyzes duplicates", function (done) {
+  it.skip("analyzes duplicates", function (done) {
+    this.timeout(EXTENDED_TIMEOUT);
+
     duplicates({
       code: basicFixture,
+      format: "json",
+      minified: true,
+      gzip: true
+    }, done);
+  });
+
+  it("analyzes suspicious patterns", function (done) {
+    this.timeout(EXTENDED_TIMEOUT);
+
+    pattern({
+      code: badBundleFixture,
+      suspectPatterns: true,
       format: "json",
       minified: true,
       gzip: true
     }, function (err, result) {
-      // console.log(result);
-      done();
+      if (err) { done(err); }
+      checkForErrors(done, function () {
+        expect(result).to.be.truthy;
+        expect(JSON.parse(result).meta.numMatches).to.equal(1);
+      });
     });
-  }).timeout(EXTENDED_TIMEOUT);
+  });
 
-  it("analyzes duplicates", function (done) {
-    duplicates({
-      code: basicFixture,
-      format: "json",
-      minified: true,
-      gzip: true
-    }, done);
-  }).timeout(EXTENDED_TIMEOUT);
+  it.skip("analyzes suspicious parses", function (done) {
+    this.timeout(EXTENDED_TIMEOUT);
 
-  it("analyzes suspicious patterns", function (done) {
-    pattern({
-      code: basicFixture,
-      format: "json",
-      minified: true,
-      gzip: true
-    }, done);
-  }).timeout(EXTENDED_TIMEOUT);
-
-  it("analyzes suspicious parses", function (done) {
     parse({
       code: basicFixture,
       format: "json",
       minified: true,
       gzip: true
     }, done);
-  }).timeout(EXTENDED_TIMEOUT);
+  });
 
   it("analyzes suspicious files", function (done) {
+    this.timeout(EXTENDED_TIMEOUT);
+
     files({
-      code: basicFixture,
+      code: badBundleFixture,
+      suspectFiles: true,
       format: "json",
       minified: true,
       gzip: true
-    }, done);
-  }).timeout(EXTENDED_TIMEOUT);
+    }, function (err, result) {
+      if (err) { done(err); }
+      checkForErrors(done, function () {
+        expect(result).to.be.truthy;
+        expect(JSON.parse(result).meta.numMatches).to.equal(3);
+      });
+    });
+  });
 
-  it("analyzes version skews", function (done) {
+  it.skip("analyzes version skews", function (done) {
+    this.timeout(EXTENDED_TIMEOUT);
+
     versions({
-      code: basicFixture,
+      code: badBundleFixture,
       root: process.cwd(),
       format: "json",
       minified: true,
       gzip: true
     }, done);
-  }).timeout(EXTENDED_TIMEOUT);
+  });
 
   it("analyzes bundle sizes", function (done) {
+    this.timeout(EXTENDED_TIMEOUT);
+
     sizes({
       code: basicFixture,
       format: "json",
       minified: true,
       gzip: true
-    }, done);
-  }).timeout(EXTENDED_TIMEOUT);
+    }, function (err, json) {
+      if (err) { done(err); }
+      checkForErrors(done, function () {
+        expect(json).to.be.truthy;
+        var result = JSON.parse(json);
+        expect(result.sizes).to.have.lengthOf(4);
+      });
+    });
+  });
 });
