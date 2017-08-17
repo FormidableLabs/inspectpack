cb9f869fa19b6408ca7134c8f9224a545c352b92
Ryan Roemer <ryan@loose-bits.com>
2017-08-17 14:32:40 -0700

    More validation

--- test/playbook.spec.js
+++ test/playbook.spec.js
@@ -67,7 +67,7 @@ describe("playbook", () => {
       format: "object",
       minified: false,
       gzip: false
-    }, (err, result) => {
+    }, (err) => {
       expect(err)
         .to.be.ok.and
         .to.have.property("message")
@@ -106,6 +106,9 @@ describe("playbook", () => {
       });
     });
 
+    // TODO(RYAN): Update fileName for delegated file reference?
+    // - https://github.com/FormidableLabs/inspectpack/issues/36
+    // - https://github.com/FormidableLabs/inspectpack/issues/37
     it("parses consuming bundles", (done) => {
       sizes({
         code: fixtures.sharedLibs.app1,
@@ -113,13 +116,24 @@ describe("playbook", () => {
         minified: false,
         gzip: false
       }, (err, result) => {
-        console.log(err, result);
+        finishAsserts(done, err, () => {
+          expect(result).to.have.property("sizes").that.has.lengthOf(3);
 
-        done();
+          const codes = result.sizes;
+          expect(codes[0]).to.have.property("id", "0");
+          expect(codes[0]).to.have.property("fileName",
+            "delegated ./foo.js from dll-reference lib_00d73d25eef8ddd2ed11");
+          expect(codes[0]).to.have.property("type", "code");
+
+          expect(codes[1]).to.have.property("id", "1");
+          expect(codes[1]).to.have.property("fileName", "external \"lib_00d73d25eef8ddd2ed11\"");
+          expect(codes[1]).to.have.property("type", "code");
+
+          expect(codes[2]).to.have.property("id", "2");
+          expect(codes[2]).to.have.property("fileName", "./app1.js");
+          expect(codes[2]).to.have.property("type", "code");
+        });
       });
     });
-
   });
-
-
 });
