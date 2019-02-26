b702591a2820d3e6453f6996c9c906af3093b752
Ryan Roemer <ryan.roemer@formidable.com>
2019-02-26 11:21:54 -0800

    Remove unneeded resolve too

--- test/fixtures/hidden-app-roots/webpack.config.js
+++ test/fixtures/hidden-app-roots/webpack.config.js
@@ -6,7 +6,6 @@ module.exports = (webpack, config) => {
     config.entry = {
       bundle: "./packages/hidden-app/src/index.js"
     };
-    config.resolve = config.resolve || {};
   }
 
   return config;
