e7cb75175fef75a43f42046d34b6c9fbfd665205
Ryan Roemer <ryan.roemer@formidable.com>
2019-07-10 05:41:43 -0700

    WIP: Update wwebpack/cli v4. Some minor broken tests for new field + tree shaking scenario appears different.

--- test/fixtures/packages/webpack4/index.js
+++ test/fixtures/packages/webpack4/index.js
@@ -5,16 +5,4 @@ if (process.env.DEBUG) {
   console.log(`webpack version: ${require("webpack/package.json").version}`);
 }
 
-// webpack4 is an `optionalDependencies` and only supported on node6.
-// Skip the build if it fails to install.
-try {
-  // eslint-disable-next-line import/no-unresolved,global-require
-  module.exports = require("webpack-cli/bin/webpack");
-} catch (err) {
-  if (err.code === "MODULE_NOT_FOUND") {
-    console.log("skipping webpack4"); // eslint-disable-line no-console
-    module.exports = null;
-  } else {
-    throw err;
-  }
-}
+module.exports = require("webpack-cli/bin/cli");
