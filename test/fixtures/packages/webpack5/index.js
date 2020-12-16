f21894bc651813685072a3beafd26ec97d0997f4
Ryan Roemer <ryan.roemer@formidable.com>
2020-12-16 15:44:35 -0800

    Add new fixtures

---
+++ test/fixtures/packages/webpack5/index.js
@@ -0,0 +1,9 @@
+
+
+if (process.env.DEBUG) {
+  // eslint-disable-next-line no-console
+  console.log(`webpack version: ${require("webpack/package.json").version}`);
+}
+
+// eslint-disable-next-line import/no-unresolved
+module.exports = require("webpack-cli");
